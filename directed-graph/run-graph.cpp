979dc1d9ed47100ddf201751f2db05c2e1b5a8ad
Stein Oskar Hinderaker <stein.oskar.hinderaker@gmail.com>
2022-11-10 15:32:43 +0100

    Double query

--- directed-graph/run-graph.cpp
+++ directed-graph/run-graph.cpp
@@ -1,3 +1,4 @@
+#include <typeinfo>
 #include <cassert>
 #include <fstream>
 #include <iostream>
@@ -5,6 +6,22 @@
 
 #include "graph.h"
 #include "query.h"
+
+int find_instances(char* file, graph::Graph g)
+{
+   std::ifstream inquery(file);
+   if (!inquery)
+   {
+      std::cerr << "Error! File " << file << " cannot be read.\n";
+      return EXIT_FAILURE;
+   }
+   graph::Query q;
+   inquery >> q;
+   inquery.close();
+
+   g.query(&q, &std::cout); // apply query q to graph g
+   return 0;
+}
 
 int main(int argc, char** argv)
 {
@@ -20,16 +37,10 @@ int main(int argc, char** argv)
    indata >> g;
    indata.close();
    // std::cout << "\nContent of graph g:\n" << g << "\n";
-   
-   std::ifstream inquery(argv[2]);
-   if(!inquery)
-   {
-      std::cerr << "Error! File " << argv[1] << " cannot be read.\n";
-      return EXIT_FAILURE;
-   }
-   graph::Query q;
-   inquery >> q;
-   inquery.close();
-   
-   g.query(&q, &std::cout);  // apply query q to graph g
+
+   std::cout << "\n Query 1: \n";
+   find_instances(argv[2], g);
+
+   std::cout << "\n Query 2: \n";
+   find_instances(argv[3], g);
 }
