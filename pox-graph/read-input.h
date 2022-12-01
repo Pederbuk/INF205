03e235a262fb25c63d46d11224c90e672019f03d
Stein Oskar Hinderaker <stein.oskar.hinderaker@gmail.com>
2022-12-01 14:51:08 +0100

    Read-input doc

--- pox-graph/read-input.h
+++ pox-graph/read-input.h
@@ -1,22 +1,27 @@
 #include "graph.h"
 #include "query.h"
 
+
 int read_query(std::string file_name, graph::Query paths[2])
 {
+   // Open file
    std::ifstream inquery(file_name);
    if (!inquery)
    {
       std::cerr << "Error! File " << file_name << " cannot be read.\n";
       return EXIT_FAILURE;
    }
+
+   // Read the first query
    graph::Query q;
    inquery >> q;
    paths[1] = q;
 
-
+   // Go to the next line
    std::string s = "";
    std::getline(inquery, s);
 
+   // Read the second query
    graph::Query p;
    inquery >> p;
    paths[0] = p;
@@ -25,14 +30,18 @@ int read_query(std::string file_name, graph::Query paths[2])
    return 0;
 }
 
+
 int read_graph(std::string file_name, graph::Graph* graph)
 {
+   // Open file
    std::ifstream indata(file_name);
    if(!indata)
    {
       std::cerr << "Error! File " << file_name << " cannot be read.\n";
       return EXIT_FAILURE;
    }
+
+   // Read the graph
    graph::Graph g;
    indata >> g;
    *graph = g;
