8310d089361d99f8655331452ee39c87a80bdc7b
Stein Oskar Hinderaker <stein.oskar.hinderaker@gmail.com>
2022-12-01 14:43:52 +0100

    Documentation to benchmark and run-graph files

--- pox-graph/benchmark.cpp
+++ pox-graph/benchmark.cpp
@@ -43,26 +43,32 @@ auto time_it(auto g, auto paths, std::string algo)
 
 int main()
 {
+   // Create graph and queries
    graph::Query paths[2];
    graph::Graph g;
    
-   std::string dir = "data/solution/";
+   // Directory of the graph and queries
+   std::string dir = "data/solution_mix/";
    
-   // Create file
+   // Create file to store the results
    std::ofstream file;
    file.open("data/benchmark.csv");
    file << "nodes,edges,edges_para,nodes_para\n";
    
-   int solution = 0;
-   // Loop files
+   int solutions = 0;   // Number of graphs with solutions
+   
+   // Loop through all graphs
    for (int i = 1; i <= 1000; i++) {
+
+      // Create file names
       std::string file_g = dir + "b" + std::to_string(i) + "_g.dat";
       std::string file_q = dir + "b" + std::to_string(i) + "_q.dat";
 
+      // Read graph and query from file
       read_graph(file_g, &g);
       read_query(file_q, paths);
       
-
+      // Variables to store the results
       int node_sum = 0;
       int edge_sum = 0;
       int node_para_sum = 0;
@@ -71,7 +77,8 @@ int main()
 
       std::cout << "Running: " << file_g << "\n";
 
-      for (int j = 0; j < 3; j++)
+      // Run the algorithms 5 times
+      for (int j = 0; j < 5; j++)
       {
          node_sum += time_it(g, paths, "nodes");
          edge_sum += time_it(g, paths, "edges");
@@ -80,13 +87,15 @@ int main()
          runs++;
       }
 
+      // Append the results to file
       file << node_sum / runs << "," << edge_sum / runs << "," << edges_para_sum / runs << "," << node_para_sum / runs << "\n";
 
+      // Check if the graph has a solution
       std::string result = g.check_two_queries_by_edges(&paths[0], &paths[1], &std::cout);
       if (result != "") {
-         solution++;
+         solutions++;
       }
-      std::cout << "Solutions: " << solution << "\n";
+      std::cout << "Solutions: " << solutions << "\n";
       }
 
    return 0;
